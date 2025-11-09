9c646f3be8946258c9b17bb1268e8cd1f96585f4
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2025-11-09 17:33:28 +0530

    firedetector

---
+++ firedetector/prediction_on_video/predictdisaster.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.models import load_model
+from firedetector import config
+import numpy as np
+import cv2
+
+
+class PredictDisaster:
+    def __init__(self):
+        self.model = load_model(config.MODEL_PATH)
+        self.W = None
+        self.H = None
+    
+    def predict(self, frame):
+        print("[INFO] loading model and label binarizer...")
+        model = load_model(config.MODEL_PATH)
+        
+        if self.W is None or self.H is None:
+            (self.H, self.W) = frame.shape[:2]
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (224, 224))
+        frame = frame.astype("float32")
+
+        preds = model.predict(np.expand_dims(frame, axis=0))[0]
+
+        return preds
